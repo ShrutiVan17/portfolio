b9e3be9b4004505291738a81dfcd20e6afd2bb64
ShrutiVan17 <shrutivanparia171@gmail.com>
2026-08-30 13:01:43 -0500

    Move StaySignal to dedicated repository

--- staysignal/src/staysignal/__init__.py
+++
@@ -1,7 +0,0 @@
-"""StaySignal hotel revenue intelligence toolkit."""
-
-from .metrics import calculate_kpis
-from .model import train_cancellation_model
-
-__all__ = ["calculate_kpis", "train_cancellation_model"]
-
